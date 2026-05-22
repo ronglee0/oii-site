14207138147a9d30f20e0a785797eb6808ad8cdd
Oii <ronglee49@gmail.com>
2026-05-23 02:10:00 +0800

    modify music

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Oii's Blog",
 
 	// 站点 URL
-	site_url: "https://blog.745566.xyz",
+	site_url: "https://blog.745566.xyz/",
 
 	// 站点描述
 	description:
