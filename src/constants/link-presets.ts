3130fb0a65ac083604704dbda8f0b8241a7f7910
Oii <ronglee49@gmail.com>
2026-05-29 17:05:11 +0800

    Fix memos icon, increase text size, enable fancybox for images

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -46,6 +46,6 @@ export const LinkPresets: { [key in LinkPreset]: NavBarLink } = {
 	[LinkPreset.Memos]: {
 		name: i18n(I18nKey.memos),
 		url: "/memos/",
-		icon: "material-symbols:edit-note",
+		icon: "fa7-regular:note-sticky",
 	},
 };
